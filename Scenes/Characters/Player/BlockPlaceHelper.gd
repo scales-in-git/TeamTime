912b8521849b9bfefb7ed072c46a155fb9ec8c9f
HalifaxLarose <halifaxlarose@gmail.com>
2025-03-15 11:06:34 -0300

    Blocks query collision

--- Scenes/Characters/Player/BlockPlaceHelper.gd
+++ Scenes/Characters/Player/BlockPlaceHelper.gd
@@ -1,24 +1,15 @@
 extends Node
 
 
+@onready var block_collision_detector: Area2D = $%BlockPlaceCollision
 @onready var good_indicator: ColorRect = $%PlaceGoodIndicator
 @onready var bad_indicator: ColorRect = $%PlaceBadIndicator
 
 const TILE_SIZE = 100
 
-func _input(event):
-	pass
-	# if event is InputEventMouseMotion:
-	# 	var camera = get_viewport().get_camera_2d()
-	# 	good_indicator.global_position = camera.get_global_mouse_position() 
-	# 	print(good_indicator.global_position)
-		# click.emit(camera.get_global_mouse_position())
+var can_place_block := false
 
-
-	return
-
-
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var camera = get_viewport().get_camera_2d()
 	# good_indicator.global_position = floor(camera.get_global_mouse_position())
 	var new_pos: Vector2 = floor(camera.get_global_mouse_position())
@@ -28,16 +19,15 @@ func _process(_delta: float) -> void:
 	new_pos.x -= new_pos.x as int % TILE_SIZE
 	new_pos.y -= new_pos.y as int % TILE_SIZE
 
-	# good_indicator.global_position.x -= TILE_SIZE if good_indicator.global_position.x
 
-	print(new_pos)
-	good_indicator.global_position = new_pos
+	block_collision_detector.global_position = new_pos
 
-	# good_indicator.global_position.x -= good_indicator.global_position.x as int % TILE_SIZE
-	# good_indicator.global_position.y -= good_indicator.global_position.y as int % TILE_SIZE
-
-	# good_indicator.global_position.x -= good_indicator.global_position.x % TILE_SIZE
-	# good_indicator.global_position.y -= good_indicator.global_position.y % TILE_SIZE
-	# good_indicator.global_position -= good_indicator.global_position % TILE_SIZE
+	if block_collision_detector.has_overlapping_bodies():
+		good_indicator.visible = false
+		bad_indicator.visible = true
+	else:
+		good_indicator.visible = true
+		bad_indicator.visible = false
+	can_place_block = good_indicator.visible
 
 	return
