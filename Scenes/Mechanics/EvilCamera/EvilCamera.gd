e0db60fc7677867c54a72f263fd5b4625b529670
HalifaxLarose <halifaxlarose@gmail.com>
2025-03-16 16:31:07 -0300

    Cameras can blink

--- Scenes/Mechanics/EvilCamera/EvilCamera.gd
+++ Scenes/Mechanics/EvilCamera/EvilCamera.gd
@@ -17,32 +17,46 @@ var blinking := false
 var turned_off := false
 var killing := false
 
+var auto_blinking_on_state_timer: SceneTreeTimer
+
 # !blinking and !turned_off = on, on animation
 # !blinking and turned_off = off, off animation
 # blinking and !turned_off = off, blink animation
 # blinking and turned_off = off, off animation, blink gets ignored
 
-func blink(how_long: float):
+func blink(how_long: float = 1.0):
 	if turned_off or killing:
 		return
 	blinking = true
-	var timer := get_tree().create_timer(how_long)
 	light.enabled = false
-	timer.play()
+	real_camera.animation = "blink"
+
+	var timer := get_tree().create_timer(how_long)
 	await timer.timeout
 	if not turned_off:
 		light.enabled = true
 		blinking = false
+		real_camera.animation = "on"
+		if auto_blink_on > 0.0:
+			auto_blinking_on_state_timer = _create_blink_timer() 
 
 func turn_off():
+	real_camera.animation = 'off'
 	turned_off = true
 	light.enabled = false
 
 func turn_on():
 	turned_off = false
 	light.enabled = true
+	real_camera.animation = 'on'
 
 
+func _create_blink_timer():
+	var timer = get_tree().create_timer(auto_blink_on)
+	timer.timeout.connect(func ():
+		blink(auto_blink_off)
+	)
+	return timer
 
 func _physics_process(_delta):
 	if blinking or turned_off or killing:
@@ -58,3 +72,7 @@ func _physics_process(_delta):
 
 			await fade_out.fade_out().finished
 			SceneTransition.call_deferred("reset_scene")
+
+func _ready():
+	if auto_blink_on > 0.0:
+		auto_blinking_on_state_timer = _create_blink_timer()
