80149c341c3fbd8b59e35f11c342e51ee1cfdcd6
HalifaxLarose <halifaxlarose@gmail.com>
2025-03-19 19:14:00 -0300

    Fixed jank with block detector and onoffstate

--- Scenes/Mechanics/BlockDetector/block_detector.gd
+++ Scenes/Mechanics/BlockDetector/block_detector.gd
@@ -9,20 +9,13 @@ extends Node2D
 func _on_block_detector_zone_area_exited(_area: Node2D):
     if not on_off_state_manager.on:
         return
-    on_off_state_manager.turn_off()
+    on_off_state_manager.call_deferred("turn_off")
 
 
 func _on_block_detector_zone_area_entered(_area: Node2D):
     if on_off_state_manager.on:
         return 
-    on_off_state_manager.turn_on()
-
-
-func _physics_process(delta):
-    for detected_body in block_detector.get_overlapping_bodies():
-        if detected_body is PlacedBlock:
-            print("Helloy")
-
+    on_off_state_manager.call_deferred("turn_on")
 
 func _ready():
     on_off_state_manager.init()
