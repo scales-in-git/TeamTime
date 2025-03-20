4da7d9c84a1ff001bf02ed6c9feb3b7f5767364e
Scales <scalesinemail@gmail.com>
2025-03-20 08:02:37 -0600

    Pause menu done

--- Scenes/Menus/pause_menu.gd
+++ Scenes/Menus/pause_menu.gd
@@ -1,28 +1,31 @@
 extends Control
 
 @export var main_menu:PackedScene
-
-
-func back_to_pause_menu():
-	$%PauseMenu.visible = true
-	$%SettingsMenu.visible = false
-
-func _process(_delta):
-	pass
-	#if Input.is_action_just_pressed('system_quit'):
-		#get_tree().quit()
-
+	
 func _ready():
+	$%ResumeButton.pressed.connect(func():back_to_game())
 	$%QuitButton.pressed.connect(func():
 		get_tree().change_scene_to_packed(main_menu)
 	)
 	$%SettingsButton.pressed.connect(func():
-		$%PauseMenu.visible = false
+		$%MainMenu.visible = false
 		$%SettingsMenu.visible = true
 	)
 
-	$%BackFromCredits.pressed.connect(back_to_pause_menu)
+	$%BackFromCredits.pressed.connect(back_to_main_menu)
+	back_to_game()
 
-func _enter_tree():
-##Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"): #Built-In action for ESC 
+		get_tree().paused=!$%PauseMenu.visible
+		back_to_main_menu()
+		$%PauseMenu.visible=(!$%PauseMenu.visible)
+
+func back_to_game():
+	back_to_main_menu()
+	$%PauseMenu.visible=false
+	get_tree().paused=false
+
+func back_to_main_menu():
+	$%MainMenu.visible = true
+	$%SettingsMenu.visible = false
