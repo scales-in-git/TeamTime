6e123067ae5ef05bc4c46a40d6999866d95da03c
HalifaxLarose <halifaxlarose@gmail.com>
2025-03-19 21:26:01 -0300

    Bullets that kill and murder you

--- Scenes/Mechanics/TurretAdaptive/AdaptiveTurret.gd
+++ Scenes/Mechanics/TurretAdaptive/AdaptiveTurret.gd
@@ -1,14 +1,16 @@
 class_name AdaptiveTurret
 extends Node2D
 
+var _TurretBullet := preload("uid://du8sp6cb7hlgr")
+
 # In degrees
 @export var rotation_limit: float = 75
 # In rads per section
 @export_range(0.0, 5.0) var rotation_speed: float = 0.5
 # In seconds
-@export var fire_rate: float = 8.0
+@export var fire_rate: float = 12.0
 # In 100 pixels per second
-@export var bullet_speed: float = 10.0
+@export var bullet_speed: float = 30.0
 
 @export var activation_zone: Area2D
 @export var state_manager: OnOffState
@@ -16,6 +18,8 @@ extends Node2D
 var targetting = null
 
 @onready var pivot: Node2D = $%BarrelPivot
+@onready var bullet_spawn_location: Marker2D = $%BulletSpawn
+@onready var unfun_timer: Timer = $%UnfunTimer
 
 func get_pointing_direction() -> Vector2:
 	var barrel_rotation = pivot.rotation
@@ -41,15 +45,31 @@ func rotate_towards(target: Vector2, delta: float):
 	pivot.rotation = clampf(pivot.rotation, Vector2.DOWN.rotated(-real_limit).angle(), Vector2.DOWN.rotated(real_limit).angle())
 	
 
+func fire_bullet():
+	if not unfun_timer.is_stopped():
+		return
+
+	var bullet = _TurretBullet.instantiate() as TurretBullet
+
+	bullet.global_position = bullet_spawn_location.global_position
+
+	var bullet_direction = Vector2.RIGHT.rotated(pivot.rotation)
+	bullet.linear_velocity = bullet_direction*bullet_speed*100
+
+	add_sibling(bullet)
+	unfun_timer.start()
 
 func _physics_process(delta):
 	if targetting:
 		rotate_towards(to_local(Game.player.global_position), delta)
+		fire_bullet()
 	else:
 		rotate_towards(Vector2.DOWN, delta)
 
 func _ready():
 	assert(abs(rotation_limit) < 90, "Turret rotation limit must be less than 90 degrees")
+
+	unfun_timer.wait_time = 1.0/fire_rate
 
 	activation_zone.body_entered.connect(func (player: Node2D):
 		if player is Player:
