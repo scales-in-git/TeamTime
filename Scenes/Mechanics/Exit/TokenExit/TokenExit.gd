70be144a00b7157df5eecbf85d88202a13893397
HalifaxLarose <halifaxlarose@gmail.com>
2025-03-23 12:47:10 -0300

    Finalized token exit

--- Scenes/Mechanics/Exit/TokenExit/TokenExit.gd
+++ Scenes/Mechanics/Exit/TokenExit/TokenExit.gd
@@ -6,6 +6,7 @@ extends BaseExit
 @export_multiline var message: String
 
 func _on_interact_zone_body_entered(body: Node2D):
+	GlobalMusic.stop()
 	if body is not Player:
 		return
 	SceneTransition.background_load_transition(scene)
