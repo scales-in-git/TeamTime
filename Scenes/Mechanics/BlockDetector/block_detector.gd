7dde35e5c5e072e4b2e03f499567ed219a02a56d
HalifaxLarose <halifaxlarose@gmail.com>
2025-03-23 13:15:37 -0300

    block detector sounds

--- Scenes/Mechanics/BlockDetector/block_detector.gd
+++ Scenes/Mechanics/BlockDetector/block_detector.gd
@@ -10,12 +10,13 @@ func _on_block_detector_zone_area_exited(_area: Node2D):
 	if not on_off_state_manager.on:
 		return
 	on_off_state_manager.call_deferred("turn_off")
-
+	$off_sound.play()
 
 func _on_block_detector_zone_area_entered(_area: Node2D):
 	if on_off_state_manager.on:
 		return 
 	on_off_state_manager.call_deferred("turn_on")
+	$on_sound.play()
 
 func _ready():
 	if on_off_state_manager:
