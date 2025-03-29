390e23bc51a356506cb1640f17e61f7f9a978c9f
HalifaxLarose <halifaxlarose@gmail.com>
2025-03-29 18:47:33 -0300

    Multiple Audio Selects

--- Scenes/Menus/Settings/settings_menu.gd
+++ Scenes/Menus/Settings/settings_menu.gd
@@ -1,18 +1,21 @@
 extends MarginContainer
 
 func set_volume(value):
-	AudioServer.set_bus_volume_db(0, linear_to_db(value))
-	$%VolumeLabel.text = "%d%%" % (value*100)
+	pass
+	#AudioServer.set_bus_volume_db(0, linear_to_db(value))
+	#$%VolumeLabel.text = "%d%%" % (value*100)
 
 
 func reset():
-	set_volume(.56)
-	$%VolumeSlider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+	pass
+	#set_volume(.56)
+	#$%VolumeSlider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
 
 
 func _ready():
-	$%VolumeSlider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
-	$%VolumeLabel.text = "%d%%" % ($%VolumeSlider.value*100)
-	$%VolumeSlider.value_changed.connect(set_volume)
-	
-	$%Reset.pressed.connect(reset)
+	pass
+	#$%VolumeSlider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+	#$%VolumeLabel.text = "%d%%" % ($%VolumeSlider.value*100)
+	#$%VolumeSlider.value_changed.connect(set_volume)
+	#
+	#$%Reset.pressed.connect(reset)
